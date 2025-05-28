353e471236411c4040dd1cb52ea0b26b8a740559
jeannemjf <jeanne.majoufre@etu.estia.fr>
2025-05-28 14:16:49 +0200

    print_pixel

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,42 @@
 #include <estia-image.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "utils.h"
 
-/**
- * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
- * 
- */
+
+ pixelRGB * get_pixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y){
+    if (data==NULL){
+       return NULL; 
+    } 
+    if (x>= width ){
+        return NULL;
+    }
+    if (y>= height){
+        return NULL;
+    }
+      return (pixelRGB *) &data[(y*width + x)*n] ;
+}
+
+void print_pixel(char *filename, int x, int y) {
+    unsigned char *data;
+    int width, height, channel_count;
+    
+
+    if (read_image_data(filename, &data, &width, &height, &channel_count) != NULL){
+        printf("Erreur lors de la lecture de l'image\n");
+        return;
+    }
+
+    pixelRGB *pixel = get_pixel(data, width, height, channel_count, x, y);
+    
+    if (pixel == NULL){
+        printf("Erreur : pixel hors limites\n");
+    } else {
+        printf("print_pixel (%d, %d): %d, %d, %d\n", x, y, pixel->R, pixel->G, pixel->B);
+    }
+    
+    free(data);
+}
+   
+
 
--- src/utils.h
+++ src/utils.h
@@ -1,19 +1,21 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct _pixelRGB {
+  unsigned char R;  /*Rouge*/  
+  unsigned char G;  /*Vert*/
+  unsigned char B;  /*Bleu*/   
+} pixelRGB ;
+
+pixelRGB * get_pixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y);
 /**
  * @brief Here, you have to define pixelRGB struct and its functions:
  * - pixel* getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y );
  * - void setPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y, pixelRGB* pixel);
  *
 */
-
-typedef struct _pixelRGB {
-  /*
-   * TO COMPLETE
-   */
-   char R ; 
-} pixelRGB ;
-
+void print_pixel(char *filename, int x, int y);
 
 #endif
