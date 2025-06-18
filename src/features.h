e302dbf811b90c3f367c833ea00e6edb30f30b78
jeannemjf <jeanne.majoufre@etu.estia.fr>
2025-06-18 15:03:59 +0200

    Jeanne-stat_report-features.h

--- src/features.h
+++ src/features.h
@@ -25,4 +25,5 @@ void max_pixel(char *source_path);
 void min_component(char *source_path, char component);
 void min_pixel(char *source_path);
 void max_component(char *source_path, char component);
+void stat_report(char *source_path);
 #endif
