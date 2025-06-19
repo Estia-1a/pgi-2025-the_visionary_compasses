57117022b6fad05f28ccb936a21e4007a790b1f9
jeannemjf <jeanne.majoufre@etu.estia.fr>
2025-06-19 16:32:09 +0200

    Jeanne-max_min_pixel-main.c

--- src/main.c
+++ src/main.c
@@ -157,6 +157,16 @@ if (strncmp(configuration.command, "max_component", 13) == 0) {
 if ( strncmp( configuration.command, "stat_report", 11 ) == 0 ) {
     stat_report(configuration.filenames[0]);
 }
+
+if ( strncmp( configuration.command, "max_pixel", 9 ) == 0 ) {
+    /* max_pixel() function is defined in feature.h and implemented in feature.c */
+    max_pixel(configuration.filenames[0]);
+}
+
+if ( strncmp( configuration.command, "min_pixel", 9 ) == 0 ) {
+    /* min_pixel() function is defined in feature.h and implemented in feature.c */
+    min_pixel(configuration.filenames[0]);
+}
 }
 
 
